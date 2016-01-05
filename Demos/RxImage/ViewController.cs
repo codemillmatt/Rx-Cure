fa05ed549d5011e178797e22f60aa1f4bbf26b0b
Matt Soucoup <msoucoup@codemilltech.com>
2016-01-05 17:04:16 -0500

    Finishing Demo
    Finishing the RxImage demo

--- Demos/RxImage/ViewController.cs
+++ Demos/RxImage/ViewController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using UIKit;
+using CoreGraphics;
+using System.Reactive.Linq;
 
 namespace RxImage
 {
@@ -14,13 +16,36 @@ namespace RxImage
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			var backImage = UIImage.FromBundle ("Background");
+			this.View.BackgroundColor = UIColor.FromPatternImage (backImage);
+
+			// Put the image onto the screen
+			var cow = new CowImageView();
+			cow.Frame = CGRect.FromLTRB (50, 50, 50 + cow.Image.Size.Width, 50 + cow.Image.Size.Height);
+			cow.UserInteractionEnabled = true;
+
+			this.View.AddSubview (cow);
+
+			// Grab the observable (what we want "pushed" to us)
+			var moveObservable = cow.MovementObservable;
+
+			// Apply any operations
+			var moveStream = moveObservable
+				.Select (imd => new {X = imd.ParentCoordinate.X, Y = imd.ParentCoordinate.Y})
+				.Where (l => l.X < this.View.Frame.Width / 2)
+				.Where (l => l.Y < this.View.Frame.Height / 2);
+
+			// Subscribe to the stream - nothing happens until we subscribe
+			moveStream.Subscribe (l => 
+				cow.Frame = CGRect.FromLTRB (
+				l.X, l.Y,
+				l.X + cow.Image.Size.Width,
+				l.Y + cow.Image.Size.Height
+			));
+					
 		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
-		}
 	}
 }
 
